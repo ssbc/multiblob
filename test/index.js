a90889a879dea8da10d898d7714c795c81df85e0
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-20 15:33:58 +1200

    test meta()

--- test/index.js
+++ test/index.js
@@ -21,128 +21,145 @@ while(l --) random1.push(crypto.randomBytes(1024))
 var l = 100, random2 = []
 while(l --) random2.push(crypto.randomBytes(1024))
 
-
-
 module.exports = function (alg) {
 
-function hasher (ary) {
-  var hasher = util.createHash(alg, true)
-  pull(pull.values(ary), hasher, pull.drain())
-  return util.encode(hasher.digest, alg)
-}
+  var start = Date.now()
 
-var hash1 = hasher(random1)
-var hash2 = hasher(random2)
+  function hasher (ary) {
+    var hasher = util.createHash(alg, true)
+    pull(pull.values(ary), hasher, pull.drain())
+    return util.encode(hasher.digest, alg)
+  }
 
-var blobs = Blobs({dir: dirname, hash: alg})
+  var hash1 = hasher(random1)
+  var hash2 = hasher(random2)
 
-tape('add, get, has, ls', function (t) {
+  var blobs = Blobs({dir: dirname, hash: alg})
 
-  pull(
-    pull.values(random1),
-    blobs.add(function (err, hash) {
-      if(err) throw err
-      t.equal(hash, hash1)
-      blobs.has(hash, function (_, has) {
-        t.ok(has)
-        t.end()
-      })
-    })
-  )
-
-})
-
-tape('add accepts the correct hash', function (t) {
-
-  pull(
-    pull.values(random1),
-    blobs.add(hash1, function (err, hash) {
-      if(err) throw err
-      t.equal(hash, hash1)
-      t.end()
-    })
-  )
-
-})
-
-tape('add errors with incorrect hash', function (t) {
-
-  pull(
-    pull.values(random2),
-    blobs.add(hash1, function (err, hash) {
-      t.ok(err)
-      t.equal(hash, hash2)
-      //if the hash was wrong, do not add it.
-      blobs.has(hash, function (_, has) {
-        console.log(_, has)
-        t.notOk(has)
-        t.end()
-      })
-    })
-  )
-
-})
-
-tape('has can take array', function (t) {
-  blobs.has([hash1, hash2], function (_, ary) {
-    t.deepEqual(ary, [true, false])
-    t.end()
-  })
-})
-
-tape('ls streams the list of hashes', function (t) {
-
-  pull(pull.values(random2), blobs.add(function (err) {
-    if(err) throw err
+  tape('add, get, has, ls', function (t) {
 
     pull(
-      blobs.ls(),
-      pull.collect(function (err, ary) {
-        t.deepEqual(ary.sort(), [hash1, hash2].sort())
-
-
-        pull(
-          blobs.ls({long: true}),
-          pull.collect(function (err, ary) {
-            t.notOk(err)
-            t.equal(ary.length, 2)
-            console.log(ary)
-            t.deepEqual(ary.map(function (e) {
-              t.ok(e.ts < Date.now())
-              t.equal(e.size, 102400)
-              return e.id
-            }).sort(), [hash1, hash2].sort())
-            t.end()
-          })
-        )
-
+      pull.values(random1),
+      blobs.add(function (err, hash) {
+        if(err) throw err
+        t.equal(hash, hash1)
+        blobs.has(hash, function (_, has) {
+          t.ok(has)
+          t.end()
+        })
       })
     )
 
-  }))
+  })
 
-})
+  tape('add accepts the correct hash', function (t) {
 
-//sometimes there are apis that need direct access
-//i.e. in electron.
-tape('resolve - direct access to the same file', function (t) {
-  var filename = blobs.resolve(hash1)
-  var hasher = util.createHash(alg, true)
-  pull(
-    Read(filename),
-    hasher,
-    pull.drain(null, function (err) {
-      t.notOk(err)
-      t.equal(util.encode(hasher.digest, alg), hash1)
+    pull(
+      pull.values(random1),
+      blobs.add(hash1, function (err, hash) {
+        if(err) throw err
+        t.equal(hash, hash1)
+        t.end()
+      })
+    )
+
+  })
+
+  tape('add errors with incorrect hash', function (t) {
+
+    pull(
+      pull.values(random2),
+      blobs.add(hash1, function (err, hash) {
+        t.ok(err)
+        t.equal(hash, hash2)
+        //if the hash was wrong, do not add it.
+        blobs.has(hash, function (_, has) {
+          console.log(_, has)
+          t.notOk(has)
+          t.end()
+        })
+      })
+    )
+
+  })
+
+  tape('has can take array', function (t) {
+    blobs.has([hash1, hash2], function (_, ary) {
+      t.deepEqual(ary, [true, false])
       t.end()
     })
-  )
-})
+  })
+
+  tape('meta returns {id, size, ts}', function (t) {
+    blobs.meta(hash1, function (err, meta) {
+      if(err) throw err
+      t.ok(meta)
+      t.equal(meta.id, hash1)
+      t.equal(meta.size, 100*1024)
+      t.ok(meta.ts > start)
+      t.ok(meta.ts < Date.now())
+
+      t.end()
+    })
+  })
+
+
+  tape('ls streams the list of hashes', function (t) {
+
+    pull(pull.values(random2), blobs.add(function (err) {
+      if(err) throw err
+
+      pull(
+        blobs.ls(),
+        pull.collect(function (err, ary) {
+          if(err) throw err
+          t.deepEqual(ary.sort(), [hash1, hash2].sort())
+
+
+          pull(
+            blobs.ls({long: true}),
+            pull.collect(function (err, ary) {
+              t.notOk(err)
+              t.equal(ary.length, 2)
+              console.log(ary)
+              t.deepEqual(ary.map(function (e) {
+                t.ok(e.ts < Date.now())
+                t.equal(e.size, 102400)
+                return e.id
+              }).sort(), [hash1, hash2].sort())
+              t.end()
+            })
+          )
+
+        })
+      )
+
+    }))
+
+  })
+  return
+  //sometimes there are apis that need direct access
+  //i.e. in electron.
+  tape('resolve - direct access to the same file', function (t) {
+    var filename = blobs.resolve(hash1)
+    var hasher = util.createHash(alg, true)
+    pull(
+      Read(filename),
+      hasher,
+      pull.drain(null, function (err) {
+        t.notOk(err)
+        t.equal(util.encode(hasher.digest, alg), hash1)
+        t.end()
+      })
+    )
+  })
 
 }
 
 if(!module.parent)
   module.exports('blake2s')
+
+
 
 
 

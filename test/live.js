cfd64636ed26aacf9abe6a98e786c83caada3dc7
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-20 14:21:32 +1200

    better coverage for ls, when used with live

--- test/live.js
+++ test/live.js
@@ -13,14 +13,15 @@ var osenv = require('osenv')
 var dirname = path.join(osenv.tmpdir(), 'test-multiblob')
 rimraf.sync(dirname)
 
-var l = 100, random1 = []
-while(l --) random1.push(crypto.randomBytes(1024))
+function random(l) {
+  var ary = []
+  while(l --) ary.push(crypto.randomBytes(1024))
+  return ary
+}
 
-var random2 = random1.slice().reverse()
-
-var random3 = random1.slice().sort(function (a, b) {
-  return a.readDoubleBE(0) - b.readDoubleBE(0)
-})
+var random1 = random(100)
+var random2 = random(200)
+var random3 = random(300)
 
 module.exports = function (alg) {
 
@@ -59,37 +60,87 @@ module.exports = function (alg) {
     })
   }
 
+  function toId(e) {
+    if(e.sync) return e
+    return e.id
+  }
+
   tape('live stream', function (t) {
 
+    var start = Date.now()
+
     var n = 3
-    var live3=[], live3new=[], old1live2=[],
-      live2new=[], old2live1=[], live1new=[]
+    var live3=[], live3new=[], live2new=[], live1new=[]
+    var old1live2=[], old2live1=[]
+
+    var live3_long=[], live3new_long=[], live2new_long=[], live1new_long=[]
+    var old1live2_long=[], old2live1_long=[]
+
+    var sizes = {}
 
     watch(blobs.ls({live: true}), live3)
     watch(blobs.ls({old: false}), live3new)
 
+    watch(blobs.ls({live: true, long: true}), live3_long)
+    watch(blobs.ls({old: false, long: true}), live3new_long)
+
     add(random1, function (err, hash1) {
       if(err) throw err
+      sizes[hash1] = 100*1024
+
       watch(blobs.ls({live: true}), old1live2)
       watch(blobs.ls({old: false}), live2new)
+      watch(blobs.ls({live: true, long: true}), old1live2_long)
+      watch(blobs.ls({old: false, long: true}), live2new_long)
+
       add(random2, function (err, hash2) {
         if(err) throw err
+        sizes[hash2] = 200*1024
 
         watch(blobs.ls({live: true}), old2live1)
         watch(blobs.ls({old: false}), live1new)
+        watch(blobs.ls({live: true, long: true}), old2live1_long)
+        watch(blobs.ls({old: false, long: true}), live1new_long)
 
         add(random3, function (err, hash3) {
+          if(err) throw err
+          sizes[hash3] = 300*1024
 
           pull(blobs.ls({live: true}), pull.take(4), pull.collect(function (err, old3live0) {
 
             console.log('live3', [hash1, hash2, hash3])
             t.deepEqual(live3,   [{sync: true}, hash1, hash2, hash3])
+
             t.deepEqual(old1live2,   [hash1, {sync: true}, hash2, hash3])
             t.deepEqual(old2live1,   sort([hash1, hash2]).concat([{sync: true}, hash3]))
             t.deepEqual(old3live0,   sort([hash1, hash2, hash3]).concat([{sync: true}]))
 
             t.deepEqual(live3new,   [hash1, hash2, hash3])
             t.deepEqual(live2new,   [hash2, hash3])
+
+            t.deepEqual(old1live2_long.map(toId),
+              [hash1, {sync: true}, hash2, hash3])
+            t.deepEqual(old2live1_long.map(toId),
+              sort([hash1, hash2]).concat([{sync: true}, hash3]))
+//            t.deepEqual(old3live0_long.map(toId),
+//              sort([hash1, hash2, hash3]).concat([{sync: true}]))
+
+            t.deepEqual(live3new_long.map(toId),   [hash1, hash2, hash3])
+            t.deepEqual(live2new_long.map(toId),   [hash2, hash3])
+
+            function checkMeta (e) {
+              if(e.sync) return
+              t.equal(e.size, sizes[e.id], 'correct size')
+              t.ok(e.ts > start, 'newer than start')
+              t.ok(e.ts < Date.now(), 'older than now')
+            }
+
+            ;[
+              live3_long, live3new_long, live2new_long,
+              live1new_long, old1live2_long, old2live1_long
+            ].forEach(function (ary) {
+              ary.forEach(checkMeta)
+            })
 
             return t.end()
           }))
@@ -100,4 +151,13 @@ module.exports = function (alg) {
 }
 
 if(!module.parent) module.exports('blake2s')
+
+
+
+
+
+
+
+
+
 

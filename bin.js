51018a1654d3392d9c78752f770114001e5b8aaa
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-21 06:42:32 +1300

    hash command

--- bin.js
+++ bin.js
@@ -5,6 +5,9 @@ var Blobs = require('./')
 var pull = require('pull-stream')
 var toPull = require('stream-to-pull-stream')
 
+var u = require('./util')
+var createHash = u.createHash
+
 var cmd = process.argv[2], arg = process.argv[3]
 
 var dir = path.join(process.env.HOME, 'tmp', 'multiblob')
@@ -32,8 +35,12 @@ if(cmd === 'has')
 if(cmd === 'ls' || cmd === 'list')
   pull(blobs.ls(), pull.drain(console.log))
 
-if(cmd === 'hash')
-  pull(toPull.source(process.stdin), createHash(console.log), pull.drain())
+if(cmd === 'hash') {
+  var hasher = createHash('sha256')
+  pull(toPull.source(process.stdin), hasher, pull.drain(null, function () {
+    console.log(hasher.digest)
+  }))
+}
 
 if(cmd === 'resolve')
   console.log(blobs.resolve(arg))
